f2a24e50d65f46ca1319ce36087a26bfe21073b3
black-desk <clx814727823@gmail.com>
2022-01-03 04:04:29 +0800

    fix(wez): improve config
      * bottom tab bar like tmux
      * almost get padding=auto

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,7 +1,40 @@
 local wezterm = require 'wezterm';
-
 local keys = require 'keys'
 local colors = require 'colors'
+
+-- FIXME how to get something like padding = auto?
+local function recompute_padding(window)
+  local window_dims = window:get_dimensions();
+  local overrides = window:get_config_overrides() or {}
+
+  if not window_dims.is_full_screen then
+    if not overrides.window_padding then
+      return;
+    end
+    overrides.window_padding = nil;
+  else
+    local new_padding = {
+      left = '0.7%',
+      right = '0.7%',
+      top = '1%',
+      bottom = 0,
+    };
+    if overrides.window_padding and new_padding.left == overrides.window_padding.left then
+      -- padding is same, avoid triggering further changes
+      return
+    end
+    overrides.window_padding = new_padding
+  end
+  window:set_config_overrides(overrides)
+end
+
+wezterm.on("window-resized", function(window, _--[[pane]])
+  recompute_padding(window)
+end);
+
+wezterm.on("window-config-reloaded", function(window)
+  recompute_padding(window)
+end);
 
 return {
   use_ime = true,
@@ -13,4 +46,16 @@ return {
   font_size = 13,
   leader = keys.leader,
   keys = keys.keys,
+  tab_bar_at_bottom = true,
+  enable_scroll_bar =false,
+  window_padding = {
+    top = 0,
+    bottom = 0,
+  },
+  window_frame = {
+    font = wezterm.font_with_fallback({
+      "FiraCode Nerd Font",
+      "Source Han Sans CN"
+    }),
+  },
 }
