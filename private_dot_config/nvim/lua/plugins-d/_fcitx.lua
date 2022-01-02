0742eb5367854a2d5b4935177ddd10ceed1cf52a
black-desk <clx814727823@gmail.com>
2022-01-03 04:27:17 +0800

    feat(nvim): add fcitx.vim

---
+++ private_dot_config/nvim/lua/plugins-d/_fcitx.lua
@@ -0,0 +1,12 @@
+-- https://github.com/lilydjwg/fcitx.vim
+
+-- Description:
+-- Keep and restore fcitx state
+
+local function config()
+end
+
+return {
+  'lilydjwg/fcitx.vim',
+  config = config,
+}
