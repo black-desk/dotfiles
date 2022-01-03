3a1bb385333a16a360f75159f2e2a5f5ceab20d1
black-desk <clx814727823@gmail.com>
2022-01-03 16:11:55 +0800

    feat(nvim): highlight url

---
+++ private_dot_config/nvim/lua/plugins-d/_url.lua
@@ -0,0 +1,12 @@
+-- https://github.com/itchyny/vim-highlighturl
+
+-- Description:
+-- highlight url
+
+local function config()
+end
+
+return {
+  'itchyny/vim-highlighturl',
+  config = config,
+}
