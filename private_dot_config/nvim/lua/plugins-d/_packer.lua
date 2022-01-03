069e799ca7119fa8e230cef5beb850e30ad0903f
black-desk <clx814727823@gmail.com>
2022-01-03 13:47:54 +0800

    feat(nvim): add packer.nvim keybindings

--- private_dot_config/nvim/lua/plugins-d/_packer.lua
+++ private_dot_config/nvim/lua/plugins-d/_packer.lua
@@ -1,1 +1,17 @@
-return 'wbthomason/packer.nvim'
+-- https://github.com/wbthomason/packer.nvim
+
+-- Description:
+-- plugin manager
+
+local function config()
+  local map = vim.api.nvim_set_keymap
+  local opts = { noremap = true, silent = true }
+
+  map('n','<leader>pc','<CMD>PackerCompile<CR>',opts)
+  map('n','<leader>ps','<CMD>PackerSync<CR>',opts)
+end
+
+return {
+  'wbthomason/packer.nvim',
+  config = config,
+}
