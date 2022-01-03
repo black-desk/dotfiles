99036eb2805865dbab1b8c43d4405e985f4c380c
black-desk <clx814727823@gmail.com>
2022-01-03 13:44:49 +0800

    fix(nvim): make lsp highlight work
      * It seems that `LspReference*` highlight group create when illuminate
        attach to client, so we have to run highlight command after that.
      * Make `LspReferenceText` bold.

--- private_dot_config/nvim/lua/lsp-d/on_attach.lua
+++ private_dot_config/nvim/lua/lsp-d/on_attach.lua
@@ -30,6 +30,9 @@ return function(client, bufnr)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
-  require 'illuminate'.on_attach(client)
+  require('illuminate').on_attach(client)
+  vim.api.nvim_command [[ hi def LspReferenceText gui=bold cterm=bold ]]
+  vim.api.nvim_command [[ hi def LspReferenceRead gui=underline,bold cterm=underline,bold ]]
+  vim.api.nvim_command [[ hi def LspReferenceWrite gui=underline,bold,italic cterm=underline,bold,italic ]]
 end
 
--- private_dot_config/nvim/lua/plugins-d/illuminate.lua
+++ private_dot_config/nvim/lua/plugins-d/illuminate.lua
@@ -4,8 +4,6 @@
 -- underline current word using lsp
 
 local function config()
-  vim.api.nvim_command [[ hi def LspReferenceRead gui=underline,bold cterm=underline,bold ]]
-  vim.api.nvim_command [[ hi def LspReferenceWrite gui=underline,bold,italic cterm=underline,bold,italic ]]
 end
 
 return {
