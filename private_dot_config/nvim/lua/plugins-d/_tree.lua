d03d97d565cbe5d626115d790c1e551ec89df0cd
black-desk <clx814727823@gmail.com>
2022-01-04 11:06:24 +0800

    fix(nvim): improve nvim-tree.lua config

--- private_dot_config/nvim/lua/plugins-d/_tree.lua
+++ private_dot_config/nvim/lua/plugins-d/_tree.lua
@@ -6,7 +6,7 @@
 local function config()
   vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
   vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-  vim.g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+  vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
   vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
   vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
   vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
@@ -17,6 +17,12 @@ local function config()
   vim.g.nvim_tree_respect_buf_cwd = 0 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
   vim.g.nvim_tree_create_in_closed_folder = 0 -- 1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
   vim.g.nvim_tree_refresh_wait = 500 -- 1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
+  vim.g.nvim_tree_show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 0,
+  }
   vim.g.nvim_tree_window_picker_exclude = {
     filetype ={
       'notify',
@@ -27,8 +33,28 @@ local function config()
       'terminal',
     }
   }
+  vim.g.nvim_tree_special_files = {
+    -- FIXME special files lose filetype related icon, disable this for now
+    --[[
+    ["README.md"] = true,
+    ["Makefile"] = true,
+    ["MAKEFILE"] = true,
+    ["CMakeLists.txt"] = true,
+    ["meson.build"] = true,
+    ["*main*"] = true,
+    ]]
+  }
   vim.g.nvim_tree_icons = {
     default = '',
+    git = {
+      unstaged = "✗",
+      staged = "✓",
+      unmerged = "",
+      renamed = "➜",
+      untracked = "★",
+      deleted = "",
+      ignored = "~"
+    },
   }
   local tree_cb = require'nvim-tree.config'.nvim_tree_callback
   local list = {
@@ -100,7 +126,7 @@ local function config()
     },
     git = {
       enable = true,
-      ignore = true,
+      ignore = false,
       timeout = 500,
     },
     view = {
@@ -113,8 +139,8 @@ local function config()
         custom_only = true,
         list = list,
       },
-      number = false,
-      relativenumber = false,
+      number = true,
+      relativenumber = true,
       signcolumn = "yes"
     },
     trash = {
