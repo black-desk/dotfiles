a127aa6ca49e80d233362f14d53cba0c37c39872
black-desk <clx814727823@gmail.com>
2022-01-07 00:34:18 +0800

    fix(nvim): update tree-sitter config

--- private_dot_config/nvim/lua/plugins-d/_treesitter.lua
+++ private_dot_config/nvim/lua/plugins-d/_treesitter.lua
@@ -6,20 +6,20 @@
 local function config()
   require'nvim-treesitter.configs'.setup {
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "maintained",
+    ensure_installed = "all",
 
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
     -- List of parsers to ignore installing
-    ignore_install = { "javascript" },
+    ignore_install = { },
 
     highlight = {
       -- `false` will disable the whole extension
       enable = true,
 
       -- list of language that will be disabled
-      disable = { "c", "rust" },
+      disable = {},
 
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
