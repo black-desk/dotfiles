d122b69e3915ff8d3274773bca2cc8ee47f1e3ea
black-desk <clx814727823@gmail.com>
2022-01-10 21:41:40 +0800

    feat(nvim): better highlight for .md

--- private_dot_config/nvim/lua/plugins-d/_onedark-vim.lua
+++ private_dot_config/nvim/lua/plugins-d/_onedark-vim.lua
@@ -1,6 +1,19 @@
 local config = function()
-  require('onedark').load()
+  local od = require('onedark')
+  od.setup({
+    highlights = {
+      TSText = {fg = '$fg'},
+      TSStrong = {fg = '$fg', fmt = 'bold'},
+      TSEmphasis = {fg = '$fg', fmt = 'italic'},
+      TSUnderline = {fg = '$fg', fmt = 'underline'},
+      TSStrike = {fg = '$fg', fmt = 'strikethrough'},
+      TSTitle = {fg = '$orange', fmt = 'bold'},
+      TSURI = {fg = '$cyan', fmt = 'underline'},
+    },
+  })
+  od.load()
 end
+
 return {
   'navarasu/onedark.nvim',
   config = config,
