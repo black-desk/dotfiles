8cb4630cd101e88f81c013193bba1fb5332a6cac
black-desk <clx814727823@gmail.com>
2022-01-19 12:06:28 +0800

    feat(nvim): add lsp_progress to lualine

--- private_dot_config/nvim/lua/plugins-d/_status.lua
+++ private_dot_config/nvim/lua/plugins-d/_status.lua
@@ -4,14 +4,22 @@
 -- status line plugin written in lua
 
 local function config()
-  require('lualine').setup({
-    options = {
-      disabled_filetypes = {
-        'NvimTree',
-        'packer',
-      },
-    },
+  local cfg = require('lualine').get_config()
+  local disable = {
+    'NvimTree',
+    'packer',
+  }
+  for _, ft in ipairs(disable) do
+    table.insert(cfg.options.disabled_filetypes, ft)
+  end
+  table.insert(cfg.sections.lualine_x,1, {
+    'lsp_progress',
+    spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' },
+
   })
+  require('lualine').setup(cfg)
+
+
 end
 
 return {
@@ -19,6 +27,7 @@ return {
   requires = {
     -- https://github.com/kyazdani42/nvim-web-devicons
     'kyazdani42/nvim-web-devicons',
+    'arkav/lualine-lsp-progress',
   },
   config = config,
 }
