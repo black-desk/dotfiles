aaf094e1eae0b5706326a849b5f106f997d169c5
black-desk <clx814727823@gmail.com>
2022-03-03 14:28:36 +0800

    feat(nvim): new way to install treesitter parsers

--- private_dot_config/nvim/lua/plugins-d/_treesitter.lua
+++ private_dot_config/nvim/lua/plugins-d/_treesitter.lua
@@ -4,18 +4,23 @@
 -- better syntax highlight
 
 local function config()
+  local parsers = require'nvim-treesitter.parsers'.maintained_parsers()
+  local extra_parsers = {
+    "markdown",
+  }
+  for _, parser in ipairs(extra_parsers) do
+    table.insert(parsers, parser)
+  end
+
   require'nvim-treesitter.configs'.setup {
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "all",
+    ensure_installed = parsers,
 
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
     -- List of parsers to ignore installing
-    ignore_install = {
-      'phpdoc',
-      'ocamllex',
-    },
+    ignore_install = {},
 
     highlight = {
       -- `false` will disable the whole extension
