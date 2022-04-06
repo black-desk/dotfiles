d2bbe37b499f3bd6f652a20b1def0dc224d9d278
black-desk <clx814727823@gmail.com>
2022-04-07 00:38:53 +0800

    feat(nvim): map fctix5-ui keys

--- private_dot_config/nvim/lua/plugins-d/_fcitx.lua
+++ private_dot_config/nvim/lua/plugins-d/_fcitx.lua
@@ -4,6 +4,27 @@
 -- Keep and restore fcitx state
 
 local function config()
+  local wk = require("which-key")
+  local key_opts = {
+    buffer  = nil, -- Global mappings.
+    silent  = true,
+    noremap = true,
+  }
+
+  wk.register(
+    {
+      ["<M-i>"] = { "<cmd>lua require'fcitx5-ui'.toggle()<cr>", "fcitx5", mode = "n" },
+    },
+    key_opts
+  )
+
+  wk.register(
+    {
+      ["<M-i>"] = { "<cmd>lua require'fcitx5-ui'.toggle()<cr>", "fcitx5",mode="i" },
+    },
+    key_opts
+  )
+
 end
 
 return {
