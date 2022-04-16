cd12d1ff9ae3bb1f3e255fec2e4e42740911975c
black-desk <clx814727823@gmail.com>
2022-04-16 21:30:48 +0800

    fix(nvim): bring back keymapping for cmp

--- private_dot_config/nvim/lua/plugins-d/_complete.lua
+++ private_dot_config/nvim/lua/plugins-d/_complete.lua
@@ -12,14 +12,14 @@ local config = function()
         vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
       ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
       ["<tab>"] = cmp.mapping.select_next_item(),
       ["<S-tab>"] = cmp.mapping.select_prev_item(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    },
+    }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'ultisnips' },
@@ -39,6 +39,7 @@ local config = function()
 
   -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
       { name = 'buffer' }
     }
@@ -46,6 +47,7 @@ local config = function()
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
       { name = 'path' }
     }, {
@@ -54,10 +56,10 @@ local config = function()
   })
 
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
 
   -- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
-  cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
+  cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
 
   Capabilities = require('cmp_nvim_lsp').update_capabilities(Capabilities)
 
