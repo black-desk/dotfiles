c56b96e5a9e2bc93fd5d387d5723714be1b8fcf4
black-desk <clx814727823@gmail.com>
2022-06-26 23:01:51 +0800

    fix(nvim): tabsize issue
      * update tabsize for filetypes that unsuitable for tab=8

--- private_dot_config/nvim/lua/common.lua
+++ private_dot_config/nvim/lua/common.lua
@@ -8,10 +8,12 @@ vim.cmd([[
         set number
         set relativenumber
         set mouse=nv
+        set nowrap
         set expandtab tabstop=8 shiftwidth=8 softtabstop=8
         set cursorline " current line
         set hlsearch   " searched key word
         set colorcolumn=81
         set signcolumn=yes
-        au Filetype gitcommit setlocal colorcolumn=72
+        au Filetype gitcommit setlocal colorcolumn=51,72 tabstop=2 shiftwidth=2 softtabstop=2
+        au Filetype markdown setlocal colorcolumn=81 tabstop=2 shiftwidth=2 softtabstop=2
 ]])
