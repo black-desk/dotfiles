a0c2f82d764bae65b34e74a4a7b8c17f73c47b9e
black-desk <me@black-desk.cn>
2023-08-23 14:05:17 +0800

    fix(wezterm): disable wayland
    gnome mutter seems to have a bug lead to wezterm crash.
    
    I disable wayland to use x11 to avoid that problem.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,40 +2,6 @@ local wezterm = require 'wezterm';
 local keys = require 'keys'
 local colors = require 'colors'
 
--- FIXME how to get something like padding = auto?
-local function recompute_padding(window)
-        local window_dims = window:get_dimensions();
-        local overrides = window:get_config_overrides() or {}
-
-        if not window_dims.is_full_screen then
-                overrides.window_padding = {
-                        top = '2.5%',
-                        bottom = 0,
-                };
-        else
-                local new_padding = {
-                        left = '0.7%',
-                        right = '0.7%',
-                        top = '0.67%',
-                        bottom = 0,
-                };
-                if overrides.window_padding and new_padding.left == overrides.window_padding.left then
-                        -- padding is same, avoid triggering further changes
-                        return
-                end
-                overrides.window_padding = new_padding
-        end
-        window:set_config_overrides(overrides)
-end
-
-wezterm.on("window-resized", function(window, _ --[[pane]])
-        recompute_padding(window)
-end);
-
-wezterm.on("window-config-reloaded", function(window)
-        recompute_padding(window)
-end);
-
 wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
         local zoomed_text = ''
         if tab.active_pane.is_zoomed then
@@ -88,4 +54,5 @@ return {
         initial_rows = 35,
         initial_cols = 120,
         disable_default_key_bindings = true,
+        enable_wayland = false,
 }
