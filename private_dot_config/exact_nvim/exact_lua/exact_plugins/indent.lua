2ac7ad2ae5c6a8accc302cb46671430660f64bf9
black-desk <me@black-desk.cn>
2023-10-07 22:48:54 +0800

    fix: update ibl

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/indent.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/indent.lua
@@ -4,11 +4,34 @@
 -- This plugin adds indentation guides to all lines (including empty lines).
 
 local function config()
-        require("indent_blankline").setup {
-                show_current_context = true,
+        local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
         }
-        vim.cmd([[ au Filetype markdown IndentBlanklineDisable ]])
 
+        local hooks = require "ibl.hooks"
+        -- create the highlight groups in the highlight setup hook, so they are reset
+        -- every time the colorscheme changes
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        end)
+
+        require("ibl").setup {
+                scope = {
+                        highlight = highlight,
+                },
+        }
 end
 
 return {
