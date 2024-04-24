7fbbd420cbd537b60a6c8e24366c88f30446ca7f
black-desk <me@black-desk.cn>
2024-04-24 10:50:58 +0800

    fix(wez): update wezterm cfg

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,37 +2,12 @@ local wezterm = require 'wezterm';
 local keys = require 'keys'
 local colors = require 'colors'
 
-wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
-        local zoomed_text = ''
-        if tab.active_pane.is_zoomed then
-                zoomed_text = ' ZOOMED'
-        end
-
-        local panes_text = ''
-
-        for _, pane_in_panes in ipairs(panes) do
-                local title = pane_in_panes.title
-                if pane_in_panes.pane_id == pane.pane_id then
-                        title = '[' .. title .. ']'
-                end
-                panes_text = panes_text .. ' ' .. title
-        end
-
-        local _, pos = string.find(pane.current_working_dir, 'file://[^/]*/')
-        if not pos then
-                pos = 0
-        end
-        local pwd = string.sub(pane.current_working_dir, pos)
-
-        return '  ⌨️  -=WezTerm=- ||' .. zoomed_text .. panes_text .. ' @ ' .. pwd
-end)
-
 return {
         use_fancy_tab_bar = false,
         use_ime = true,
         colors = colors,
         font = wezterm.font_with_fallback({
-                "FiraCode Nerd Font",
+                "Fira Code Nerd Font Mono",
                 "Noto Sans Mono CJK SC"
         }),
         font_size = 13,
@@ -46,7 +21,7 @@ return {
         },
         window_frame = {
                 font = wezterm.font_with_fallback({
-                        "FiraCode Nerd Font",
+                        "Fira Code Nerd Font Mono",
                         "Source Han Sans CN"
                 }),
         },
