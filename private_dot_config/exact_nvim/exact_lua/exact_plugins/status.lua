c15de8c63a16ea1337cf2187c6920ccb2b1d93de
black-desk <me@black-desk.cn>
2024-07-23 17:42:39 +0800

    fix(nvim): fix lualine x and y

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/status.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/status.lua
@@ -5,51 +5,25 @@
 
 local function config()
         local cfg = require('lualine').get_config()
-        local disable = {
-                'neo-tree',
-                'packer',
-        }
-        for _, ft in ipairs(disable) do
-                table.insert(cfg.options.disabled_filetypes, ft)
-        end
-        table.insert(cfg.sections.lualine_x, 1, {
+        table.insert(cfg.options.disabled_filetypes, 'neotree')
+        table.insert(cfg.sections.lualine_x, {
                 'lsp_progress',
                 spinner_symbols = {
                         '🌑 ', '🌒 ', '🌓 ', '🌔 ',
                         '🌕 ', '🌖 ', '🌗 ', '🌘 ' }
         })
-        table.insert(
-                cfg.sections.lualine_y,
-                'require("fcitx5-ui").get_current_input_method_status().name')
-        require('lualine').setup({
-                options = {
-                        disabled_filetypes = {
-                                'neo-tree',
-                                'packer',
-                        },
-                },
-                sections = {
-                        lualine_x = { {
-                                'lsp_progress',
-                                spinner_symbols = {
-                                        '🌑 ', '🌒 ', '🌓 ', '🌔 ',
-                                        '🌕 ', '🌖 ', '🌗 ', '🌘 ' }
-                        } },
-                        lualine_y = {
-                                function()
-                                        local status =
-                                            require("fcitx5-ui").get_current_input_method_status()
-                                        if status.name == nil then
-                                                return ""
-                                        end
+        table.insert(cfg.sections.lualine_y, {
+                function()
+                        local status =
+                            require("fcitx5-ui").get_current_input_method_status()
+                        if status.name == nil then
+                                return ""
+                        end
 
-                                        return status.name .. "|" .. status.language_code
-                                end
-                        },
-                }
-
-
+                        return status.name .. "|" .. status.language_code
+                end
         })
+        require('lualine').setup(cfg)
 end
 
 return {
