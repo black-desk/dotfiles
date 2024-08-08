ff141b400f4b06413743b646956b6c4d22ef368b
black-desk <me@black-desk.cn>
2024-08-08 16:28:18 +0800

    feat: add config for distrobox

---
+++ private_dot_config/exact_distrobox/executable_pre-init.sh
@@ -0,0 +1,7 @@
+OS_NAME="$(awk -F= '/^NAME=/ { print $2 }' /etc/os-release)"
+OS_VERSION_CODENAME="$(awk -F= '/^VERSION_CODENAME=/ { print $2 }' /etc/os-release)"
+
+if [ "$OS_NAME" == "\"uos\"" ] && [ "$OS_VERSION_CODENAME" == "eagle" ]; then
+        echo "Update apt sources.list to use internal mirror."
+	echo >/etc/apt/sources.list "deb [trusted=yes] http://pools.uniontech.com/desktop-professional eagle main contrib non-free"
+fi
