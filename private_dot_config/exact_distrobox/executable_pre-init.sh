52d75fceda38d295a5d6275bcb211d32cb6f69d7
black-desk <me@black-desk.cn>
2024-08-09 13:53:34 +0800

    fix: update distrobox config

--- private_dot_config/exact_distrobox/executable_pre-init.sh
+++ private_dot_config/exact_distrobox/executable_pre-init.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 OS_NAME="$(awk -F= '/^NAME=/ { print $2 }' /etc/os-release)"
 OS_VERSION_CODENAME="$(awk -F= '/^VERSION_CODENAME=/ { print $2 }' /etc/os-release)"
 
-if [ "$OS_NAME" == "\"uos\"" ] && [ "$OS_VERSION_CODENAME" == "eagle" ]; then
+if [ "$OS_NAME" = "\"uos\"" ] && [ "$OS_VERSION_CODENAME" = "eagle" ]; then
         echo "Update apt sources.list to use internal mirror."
 	echo >/etc/apt/sources.list "deb [trusted=yes] http://pools.uniontech.com/desktop-professional eagle main contrib non-free"
 fi
