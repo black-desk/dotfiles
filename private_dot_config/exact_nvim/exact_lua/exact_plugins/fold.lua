5ae47fd225ccca6e55cd201e6acb245fc42676d5
black-desk <me@black-desk.cn>
2024-08-19 09:03:40 +0800

    fix(nvim): fix fold

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/fold.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/fold.lua
@@ -37,7 +37,8 @@ local config = function()
         require('ufo').setup({
                 fold_virt_text_handler = handler
         })
-
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 end
 
 return {
