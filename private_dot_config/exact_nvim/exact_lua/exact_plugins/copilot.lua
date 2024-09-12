edb30796e5848ef6f32128b410940ec2482b6240
black-desk <me@black-desk.cn>
2024-09-12 16:43:29 +0800

    feat(nvim): ai

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/copilot.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/copilot.lua
@@ -1,4 +1,4 @@
-return {
+return { {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         event = "InsertEnter",
@@ -23,13 +23,56 @@ return {
                         end
 
                 },
-                {
-                        "CopilotC-Nvim/CopilotChat.nvim",
-                        branch = "canary",
-                        opts = {
-                                context = 'buffers'
-                        }
-                }
+                -- {
+                -- "CopilotC-Nvim/CopilotChat.nvim",
+                -- branch = "canary",
+                -- opts = {
+                -- context = 'buffers'
+                -- }
+                -- }
         }
 
-}
+}, {
+        "zbirenbaum/copilot-cmp",
+        dependencies = { "zbirenbaum/copilot.lua" },
+}, {
+
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false, -- set this if you want to always pull the latest change
+        opts = {
+                provider = "copilot",
+                sidebar_header = {
+                        rounded = false,
+                }
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+                "stevearc/dressing.nvim",
+                "nvim-lua/plenary.nvim",
+                "MunifTanjim/nui.nvim",
+                --- The below dependencies are optional,
+                "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+                "zbirenbaum/copilot.lua",      -- for providers='copilot'
+                {
+                        -- support for image pasting
+                        "HakonHarnes/img-clip.nvim",
+                        event = "VeryLazy",
+                        opts = {
+                                -- recommended settings
+                                default = {
+                                        embed_image_as_base64 = false,
+                                        prompt_for_file_name = false,
+                                        drag_and_drop = {
+                                                insert_mode = true,
+                                        },
+                                        -- required for Windows users
+                                        use_absolute_path = true,
+                                },
+                        },
+                },
+        },
+} }
