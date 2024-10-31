1b7f717180312e43e7516ce0378b26914f30d9a1
black-desk <me@black-desk.cn>
2024-10-31 17:46:00 +0800

    fix(nvim): update clangd config

--- private_dot_config/exact_nvim/exact_lua/exact_lsp_configs/clangd.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_lsp_configs/clangd.lua
@@ -3,5 +3,9 @@ return {
                 "clangd",
                 "--offset-encoding=utf-16",
                 "--completion-style=detailed",
-        }
+        },
+        root_dir = require("lspconfig.util").root_pattern(
+                "build/compile_commands.json",
+                "compile_commands.json",
+                ".git"),
 }
